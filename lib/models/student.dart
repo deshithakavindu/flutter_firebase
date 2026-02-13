e4d32b8d7fdd07806a8d4004bfffd85f5b2a3cc8
deshithakavindu <deshithakavindu2729@gmail.com>
2026-02-13 21:20:47 +0530

    object create

---
+++ lib/models/student.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Student {
+  String name;
+  String city;
+
+  Student({required this.name, required this.city});
+
+  factory Student.fromJson(Map<String, dynamic> json) {
+    return Student(name: json['name'], city: json['city']);
+  }
+}
