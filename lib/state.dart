f62b30a1231414d803cd5d1537292acced60ec75
deshithakavindu <deshithakavindu2729@gmail.com>
2026-02-23 18:23:55 +0530

    states

---
+++ lib/state.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class State_page extends StatefulWidget {
+  const State_page({super.key});
+
+  @override
+  State<State_page> createState() => _State_pageState();
+}
+
+class _State_pageState extends State<State_page> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
